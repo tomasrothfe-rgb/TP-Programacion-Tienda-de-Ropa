6b2bef07ce77de911006035b0e6f7560d0a7e342
tomasrothfe-rgb <tomasrothfe@gmail.com>
2026-09-15 16:32:59 -0300

    Comienzo de la vista Clientes
    Se creó un nuevo archivo clientes. Se agregaron funciones simples para revisar el funcionamiento (Carrito de compras y mostrar productos)

---
+++ Vistas/cliente.py
@@ -0,0 +1,104 @@
+import flet as ft
+import logging 
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from conexion_bd import creacion_bd, mostrar_productos
+
+logging.basicConfig(level=logging.INFO)
+
+
+
+
+def main(page: ft.Page):
+    page.title = "Panel de Administrador "
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.window.width = 700
+    page.window.height = 500
+
+    lista_productos=[]
+    carrito_letras=0
+
+    def agregar_al_carrito(p):
+        nonlocal lista_productos
+        nonlocal carrito_letras
+        lista_productos.append(p)
+        carrito_letras+=1
+        carrito.value=str(carrito_letras)
+        page.update()
+
+    def mostrar_pantalla_productos():
+        productos=mostrar_productos()
+        lista_derecha.content.controls.clear()
+        for producto in productos:
+               lista_derecha.content.controls.append(
+                      ft.ListTile(title=producto[1], subtitle=(f"Precio: {producto[2]}       Stock: {producto[3]}"),trailing=ft.IconButton(
+        icon=ft.Icons.PLUS_ONE,
+        icon_color=ft.Colors.BLACK,
+        on_click=lambda e, p=producto: agregar_al_carrito(p))
+    ))
+                    
+        page.update()
+
+    def mostraralgo():
+         nonlocal lista_productos
+         print(lista_productos)
+
+    
+    carrito=ft.Text(str(carrito_letras))
+    lista_derecha= ft.Container(
+           content=ft.ListView(expand=1, spacing=10, padding=20,),
+           bgcolor= ft.Colors.GREY,
+           expand=True
+    )
+
+    contenedor_principal=ft.Container(
+        content=ft.Column(
+               controls=[
+                      ft.Text("Panel de Cliente", size=30, weight="bold", color=ft.Colors.BLUE),
+                ft.Button(
+                    "Ver carrito de compras",
+                    on_click=lambda e:mostraralgo(), 
+                ),
+                ft.Button(
+                    "Comprar",
+                    on_click=lambda e:print("Comprar"),
+                ),
+                ft.Button(
+                    "Modificar Datos personales",
+                    on_click=lambda e:print("Datos"),
+                    ),
+                ft.Button(
+                    "Historial de compra",
+                    on_click=lambda e:print("Historial"),
+                )
+               ]
+        )
+    )
+
+    mostrar_pantalla_productos()
+    
+    page.add(
+        ft.Row(
+            controls=[
+                contenedor_principal,
+                lista_derecha,
+                ft.Row(
+                    controls=[
+                    carrito,
+                    ft.Icon(ft.Icons.SHOPPING_BAG)
+                ]
+    ),
+                      ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+            vertical_alignment=ft.CrossAxisAlignment.START,
+            expand=True,
+        )
+        
+    )
+
+
+ft.app(target=main)
